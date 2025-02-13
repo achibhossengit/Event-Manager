ab6b987138317c583913a1ea8fc434dd6c47956e
achibhossengit <mail.achibhossen@gmail.com>
2025-02-13 18:57:09 +0600

    basic count and task show operations done

--- event/urls.py
+++ event/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from event.views import test_view
+from event.views import test_view, dashboard
 
 urlpatterns = [
     path('test', test_view),
+    path('dashboard', dashboard, name='dashboard'),
 ]
--- event/views.py
+++ event/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render, HttpResponse, redirect
 from event.forms import EventModelForm
+from event.models import Event, Category, Participant
+from datetime import date
 
 def test_view(request):
     return render(request, 'dashboard.html')
+
+def dashboard(request):
+    type = request.GET.get('type')
+    print(type)
+    events = Event.objects.all()
+    total_participants = Participant.objects.all().count()
+    total_events = events.count()
+    upcoming_events = Event.objects.filter(date__gte=date.today()).count()
+    today_events = Event.objects.filter(date=date.today()).count()
+    past_events = Event.objects.filter(date__lt = date.today()).count()
+    context = {
+        'events':events,
+        'total_participants':total_participants,
+        'total_events':total_events,
+        'upcoming_events':upcoming_events,
+        'today_events':today_events,
+        'past_events':past_events
+    }
+    return render(request, 'dashboard.html', context)
