d8b146312dc84f309697a83a5bd47b95a63a103d
achibhossengit <mail.achibhossen@gmail.com>
2025-02-13 09:50:53 +0600

    created data models

--- event/models.py
+++ event/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.CharField()
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=200)
+    category = models.ForeignKey('Category', on_delete=models.SET_DEFAULT, default=1, related_name= 'events')
+
+class Participant(models.Model):
+    name  = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    events = models.ManyToManyField('Event', related_name= 'participants')
+    
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField()
