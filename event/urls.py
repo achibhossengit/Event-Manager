4f4d994fa94507bd1db8dfaf3c42fdeb79bd352e
achibhossengit <mail.achibhossen@gmail.com>
2025-02-13 13:37:06 +0600

    remove create view and form, setup tailwind, created header.html

--- event/urls.py
+++ event/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path, include
-from event.views import create_event
 
 urlpatterns = [
-    path('create-event', create_event, name='event_creations')
 ]
--- event/views.py
+++ event/views.py
@@ -1,13 +1,3 @@
 from django.shortcuts import render, HttpResponse, redirect
 from event.forms import EventModelForm
 
-def create_event(request):
-    if request.method == 'POST':
-        form = EventModelForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('event_creations')
-    else:
-        form = EventModelForm()
-
-    return render(request, 'create_event.html', {'form': form})
