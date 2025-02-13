9b4fb808462328ecaa1c61ee591b0405a896b33f
achibhossengit <mail.achibhossen@gmail.com>
2025-02-13 17:30:26 +0600

    basic templates file creation done

--- event/urls.py
+++ event/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from event.views import test_view
 
 urlpatterns = [
+    path('test', test_view),
 ]
--- event/views.py
+++ event/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
 from event.forms import EventModelForm
 
+def test_view(request):
+    return render(request, 'dashboard.html')
