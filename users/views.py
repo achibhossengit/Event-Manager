b334633e05a634d5bbefe451d9b3e79b3e17dd86
achibhossengit <mail.achibhossen@gmail.com>
2025-03-03 09:36:31 +0600

    create user with sign-in view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from users.forms import UserModelForm
 
 def sign_up(request):
     form = UserModelForm()
+    if request.method == 'POST':
+        form = UserModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('homepage')
     return render(request, 'sign_up.html', {'form':form})
