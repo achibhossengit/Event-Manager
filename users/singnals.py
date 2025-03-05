19f114cbffc5dccaa979ec85e762ba803f2f189a
achibhossengit <mail.achibhossen@gmail.com>
2025-03-05 21:56:51 +0600

    change apps.py of users app for loading signals

---
+++ users/singnals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User, Group
+
+@receiver(post_save, sender=User)
+# default parameer for post_save signals: sender, instance, created, update_fields(its a tuple which contain update related info like- username, password etc), raw, **kwargs(for send custom parameters)
+# default parameer for pre_save signals: sender, instance, raw, using(when used multiple database its define which database used here), **kwargs(for send custom parameters)
+def assign_default_role(sender,instance, created, **kwargs):
+    if created:
+        print('checking')
+        user_group, created = Group.objects.get_or_create(name='Participants')
+        instance.groups.add(user_group)
+        instance.save()
